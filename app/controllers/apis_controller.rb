e6eda82993ed50679774eb21cc6b5d690fac79b1
Jon Evans <jon@artfuldodgersoftware.com>
2013-01-24 01:25:58 -0700

    Add api documentation

---
+++ app/controllers/apis_controller.rb
@@ -0,0 +1,3 @@
+class ApisController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Vulgarities::Application.routes.draw do
   root to: 'vulgarities#index'
   resources :vulgarities, only: [:index]
+  resources :apis, only: [:index]
 end
