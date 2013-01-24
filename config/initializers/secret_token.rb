99954871b6feccb37fc6bc417b526fb99d96ed0d
Jon Evans <jon@artfuldodgersoftware.com>
2013-01-23 21:15:41 -0700

    Initial generated site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vulgarities::Application.config.secret_token = 'c57d1c815a5b092c7c0d078655918251782c0bb312757542bd55f9ff8097b1f41be467ed25713de3bd5eee5f78a4b42a5c29a5a5fea9cc28cf2164951ee394a3'
